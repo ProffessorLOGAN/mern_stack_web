1fb33ad0057c525e5443b9470c5cec16d6ce349c
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-03-31 09:11:04 +0530

    project backend started
    1>mongo db connect
    2>models user schema
    3>routers
    4>middleware
    5>dotenv config

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+
+
+
+// dotenv config
+
+dotenv.config({ path: "./config/config.env" });
+require("../src/db/database");
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+// using Router for user authentication
+app.use( require("./route/auth.js"));
+
+
+// middleware
+const middleware = (req, res, next) => {
+  console.log(`hello middleware`);
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("hello world , Homepage");
+});
+
+app.get("/about", middleware, (req, res) => {
+  res.send("hello world , About page ");
+});
+
+// app.get("/contact", (req, res) => {
+//   res.send("hello world , contact page ");
+// });
+
+// app.get("/signin", (req, res) => {
+//   res.send("hello world , sign in page");
+// });
+
+// app.get("/signup", (req, res) => {
+//   res.send("hello world , register page ");
+// });
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
