fb888d309215b7b8416ddb689a5c766bd0e66d2d
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-04-03 23:14:40 +0530

    login route created

--- Server/src/route/auth.js
+++ Server/src/route/auth.js
@@ -1,34 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
-require('../db/database');
-const User = require('../models/userSchema');
+require("../db/database");
+const User = require("../models/userSchema");
 
 router.get("/", (req, res) => {
   res.send(`Hello Proffessor LOGAN ,router`);
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "pLz fill properly" });
   }
+  try {
+    const userExists = await User.findOne({ email: email });
 
-  User.findOne({ email:email })
-  .then((userExists) => {
-            if(userExists){
-    return res.status(422).json({ error: "Email already Exists" });
+    if (userExists) {
+      return res.status(422).json({ error: "Email already exists" });
+    }
+
+    const user = new User({ name, email, phone, work, password, cpassword });
+    const userRegister = await user.save();
+    res.status(201).json({ message: "user registered successfully" });
+  } catch (err) {
+    console.log(err);
   }
+});
 
-  const user = new User({name, email, phone, work, password, cpassword});
- 
-  user.save().then( () => {
-    res.status(201).json({ message: "Successfully register" });
-  }).catch( (err) => res.status(500).json({ error: "failed to register" }));
+// Login route
+router.post("/signin", async (re, res) => {
+  try {
+    const { eamil, password } = req.body;
 
-  }).catch( (err) => { console.log(err); });
-
+    if(!email || !password){
+      return res.status(400).json({ error: "PLZ filled the data " });
+    }
+    const userLogin = await Usr.findOne({email:email});
+    if(!userLogin){
+      res.status(400).json({ error: "user error" });
+    }else{
+      res.json({message:"usser signin successfully"});
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
