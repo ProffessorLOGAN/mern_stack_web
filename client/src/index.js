cf1114ce2635c3a0e53990403b24156daddeede9
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-04-12 11:47:11 +0530

    index.js updated - React new updates
    >React Root
    >BrowserRouter
    
    date - 12/4/22

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+import { BrowserRouter } from "react-router-dom";
+
 
 root.render(
   <StrictMode>
